2530d238f9c3ff29305318e6e87f130386463fc5
fakhariqbal <fakhareiqbal3534@gmail.com>
2025-05-31 00:20:15 +0500

    trigger 2

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -156,7 +156,7 @@ export default function Dashboard() {
             <div className="dashboard-card">
               <div>
                 <h4 className="card-top-heading">Total Users</h4>
-                <h2>{totalUsers} people</h2>
+                <h2>{totalUsers} users</h2>
               </div>
               <img src={WaveIcon} />
             </div>
